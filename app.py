20a07cfe77c6a70e9313d751b2d46fc1cdf5c5f5
THaraSin <judothara29@gmail.com>
2021-11-18 19:40:28 +0700

    g

--- app.py
+++ app.py
@@ -2,16 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/git_update', methods=['POST'])
-def git_update():
-  repo = git.Repo('./orbe')
-  origin = repo.remotes.origin
-  repo.create_head('main', 
-  origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-  origin.pull()
-  return '', 200
-
 @app.route('/')
 def table():
-    return Hello world
+    return "Hello world"
     
